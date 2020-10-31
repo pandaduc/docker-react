0fd4125b9f91eca23a2f497378c72686d027200e
Duc Anh Nguyen <danguyen2903@gmail.com>
2020-10-31 23:09:46 +1100

    Change app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Make changes on develop branch
         </a>
       </header>
     </div>
